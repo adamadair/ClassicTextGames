1017d1a1ccc84e74e915e06b491ccd922576e0ef
Adam Adair <adamadair@outlook.com>
2018-01-30 14:13:14 -0600

    Added lunar landing BASIC files.

--- LunarLanding/lem.py
+++ LunarLanding/lem.py
@@ -1,3 +1,15 @@
+import sys
+import math
+
+
+class PlayerWin(Exception):
+    """Thrown when a player lands the lunar module."""
+
+
+class PlayerLose(Exception):
+    """Thrown when a player loses for any reason."""
+
+
 def intro():
     print(tab(34) + "LEM")
     print(tab(15) + "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY")
@@ -56,7 +68,7 @@ class Rockt2:
                 print("JUST ANSWER THE QUESTION, PLEASE, ")
         return resp
 
-    def instructions(self, q):
+    def __print_instructions(self, q):
         """
         print instructions. lines 315-480
         """
@@ -106,26 +118,30 @@ class Rockt2:
     def run(self):
         intro()
         q = self.user_choice("HAVE YOU FLOWN AN APOLLO/LEM MISSION BEFORE (YES OR NO)? ", ["yes", "no"])
-        self.K = self.get_measurement_option(q)
-        if self.K == 0:
-            self.Z = 6080
-            self.M_STR = "FEET"
-            self.G3 = 0.592
-            self.N_STR = "N.MILES"
-            self.G5 = 1000
-        else:
-            self.Z = 1852.8
-            self.M_STR = "METERS"
-            self.G3 = 3.6
-            self.N_STR = " KILOMETERS"
-            self.G5 = 1000
-        self.instructions(q)
+        self.__set_measurement_option(q)
+        self.__print_instructions(q)
+        try:
+            while True:
+                # this is the game loop
+                self.__update_lunar_module_spatial_position()
+                self.__update_and_print_output()
+                self.__check_craft_status()
+                self.__get_tpa_from_user()
 
+        except PlayerLose as end:
+            print(end)
+        except PlayerWin as end:
+            print(end)
+        except Exception as error:
+            print(error)
         # for i in range(self.n - 1):
         #    if self.M1 != 0:
 
-
-    def get_measurement_option(self, q):
+    def __set_measurement_option(self, q):
+        """
+        Allow the user to choose between metric or english measurements.
+        :param q: expecting 'yes' or 'no'
+        """
         i = -1
         if q == 'yes':
             m = input("INPUT MEASUREMENT OPTION NUMBER? ")
@@ -142,13 +158,166 @@ class Rockt2:
                 i = int(m)
             except:
                 pass
-        return i
+        self.K = i
+        if self.K == 0:
+            self.Z = 6080
+            self.M_STR = "FEET"
+            self.G3 = 0.592
+            self.N_STR = "N.MILES"
+            self.G5 = 1000
+        else:
+            self.Z = 1852.8
+            self.M_STR = "METERS"
+            self.G3 = 3.6
+            self.N_STR = " KILOMETERS"
+            self.G5 = 1000
+
+    def __get_tpa_from_user(self):
+        if self.M1 <= 0:
+            # no fuel so just return defaults
+            self.T1 = 20
+            self.F = 0.0
+            self.P = 0.0
+            return
+        print()
+        tpa = None
+        while tpa is None:
+            tpa = input("T,P,A? ").split(',')
+            if len(tpa) != 3:
+                tpa = None
+                continue
+            try:
+                self.T1 = int(tpa[0])
+                self.F = float(tpa[1])
+                self.p = float(tpa[2])
+                self.F /= 100
+                if self.T1 < 0:
+                    print("\nTHIS SPACECRAFT IS NOT ABLE TO VIOLATE THE SPACE-TIME CONTINUUM.")
+                    tpa = None
+                    continue
+                if self.T1 == 0:
+                    raise PlayerLose("MISSION ABENDED")
+                abf = abs(self.F - 0.05)
+                if abf > 1 or abf < 0.05:
+                    print("IMPOSSIBLE THRUST VALUE {0}".format(self.__failed_thrust_reason(self.F)))
+                    tpa = None
+                    continue
+                if abs(self.P) > 180:
+                    print("\nIF YOU WANT TO SPIN AROUND, GO OUTSIDE THE MODULE\nFOR AN E.V.A.")
+                    tpa = None
+                    continue
+
+            except:
+                tpa = None
+
+    def __failed_thrust_reason(self, f):
+        if f < 0:
+            return "NEGATIVE"
+        if f - 0.05 < 0.05:
+            return "TOO SMALL"
+        return "TOO LARGE"
+
+    def __update_lunar_module_trajectory(self):
+        self.N = 20
+        if self.T1 >= 400:
+            self.N = self.T1 / 20
+        self.T1 /= self.N
+        self.P = self.P * 3.14159 / 180
+        self.S = math.sin(self.P)
+        self.C = math.cos(self.P)
+        self.M2 = self.M0 * self.T1 * self.F / self.B
+        self.R3 = -.5 * self.R0 * ((self.V0 / self.R) ** 2) + self.R * self.A1 * self.A1
+        self.A3 = - 2 * self.R1 * self.A1 / self.R
+
+    def __update_lunar_module_spatial_position(self):
+        for i in range(1, self.N + 1):
+            if self.M1 > 0:
+                # update the fuel situation
+                self.M1 = self.M1 - self.M2
+                if self.M1 <= 0:
+                    # ran out of fuel
+                    self.F = self.F * (1 + self.M1 / self.M2)
+                    self.M2 = self.M1 + self.M2
+                    print("YOU ARE OUT OF FUEL.")
+                    self.M1 = 0
+            else:
+                # out of fuel
+                self.F = 0
+                self.M2 = 0
+            self.M = self.M - .5 * self.M2
+
+            self.R4 = self.R3
+            self.R3 = -.5 * self.R0 * ((self.V0 / self.R) ** 2) + self.R * self.A1 * self.A1
+            self.R2 = (3 * self.R3 - self.R4) / 2 + 0.00526 * self.F1 * self.F * self.C / self.M
+            self.A4 = self.A3
+            self.A3 = -2 * self.R1 * self.A1 / self.R
+            self.A2 = (3 * self.A3 - self.A4) / 2 + 0.0056 * self.F1 * self.F * self.S / (self.M * self.R)
+            self.X = self.R1 * self.T1 + 0.5 * self.R2 * self.T1 * self.T1
+            self.R = self.R + self.X
+            self.H0 = self.H0 + self.X
+            self.R1 = self.R1 + self.R2 * self.T1
+            self.A = self.A + self.A1 * self.T1 + 0.5 * self.A2 * self.T1 * self.T1
+            self.A1 = self.A1 + self.A2 * self.T1
+            self.M = self.M - 0.5 * self.M2
+            self.T = self.T + self.T1
+            if self.H0 < 3.287828E-04:
+                return
+
+    def __update_and_print_output(self):
+        self.H = self.H0 * self.Z
+        self.H1 = self.R1 * self.Z
+        self.D = self.R0 * self.A * self.Z
+        self.D1 = self.R * self.A1 * self.Z
+        self.T2 = self.M1 * self.B / self.M0
+        print(" {0:9}{1:12} {2:13} {3:11} {4:10} {5}".format(str(self.T), str(self.H), str(self.D), str(self.H1),
+                                                             str(self.D1), str(self.T2)))
+
+    def __check_craft_status(self):
+        if self.H0 < 3.287828E-04:
+            if self.R1 < - 8.21957E-04:
+                self.__crash()
+            if abs(self.R * self.A1) > 4.93174E-04:
+                self.__crash()
+            if self.H0 < -3.287828E-04:
+                self.__crash()
+            if abs(self.D) > 10 * self.Z:
+                self.__landed()
+            self.__won()
+        if self.R0 * self.A > 164.474:
+            self.__lost_in_space()
+
+    def __crash(self):
+        msg = "CRASH !!!!!!!!!!!!!!!!"
+        msg += "\nYOUR IMPACT CREATED A CRATER {0} {1} DEEP.".format(abs(self.H), self.M_STR)
+        x1 = math.sqrt(self.D1 * self.D1 + self.H1 * self.H1) * self.G3
+        msg += "AT CONTACT YOU WERE TRAVELING {0} {1}/HR".format(x1, self.N_STR)
+        raise PlayerLose(msg)
+
+    def __lost_in_space(self):
+        raise PlayerLose("\nYOU HAVE BEEN LOST IN SPACE WITH NO HOPE OF RECOVERY.")
+
+    def __landed(self):
+        msg = "YOU ARE DOWN SAFELY - \n\n"
+        msg += "BUT MISSED THE LANDING SITE BY {0} {1}.".format(abs(self.D / self.G5), self.N_STR)
+        raise PlayerLose(msg)
+
+    def __won(self):
+        msg = "TRANQUILITY BASE HERE -- THE EAGLE HAS LANDED.\nCONGRATULATIONS -- THERE WAS NO SPACECRAFT DAMAGE."
+        msg += "\nYOU MAY NOW PROCEED WITH SURFACE EXPLORATION."
+        raise PlayerWin(msg)
 
 
 if __name__ == '__main__':
     try:
-        r = Rockt2()
-        r.instructions()
+        if sys.version_info < (3,):
+            raise Exception("LEM requires Python 3.0 or higher.")
+        run_me = True
+        while run_me:
+            r = Rockt2()
+            r.run()
+            resp = r.user_choice("DO YOU WANT TO TRY IT AGAIN (YES/NO)? ", ['yes', 'no'])
+            run_me = resp == 'yes'
+        print("\nTOO BAD, THE SPACE PROGRAM HATES TO LOSE EXPERIENCED\nASTRONAUTS.")
     except KeyboardInterrupt:
         pass
     except Exception as err:
